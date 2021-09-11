6d7e3beb113ea53f48eb4dd4f5b8648a013db180
nicolaas @ tappy <github@sunnysideup.co.nz>
2021-09-11 22:40:34 +1200

    WIP

--- src/Api/TabsBuilder.php
+++ src/Api/TabsBuilder.php
@@ -2,45 +2,24 @@
 
 namespace Sunnysideup\ModelAdminManyTabs\Api;
 
-use SilverStripe\Forms\Form;
-use SilverStripe\Forms\TabSet;
-use SilverStripe\Forms\Tab;
-
-use SilverStripe\Forms\GridField\GridField;
-use SilverStripe\Forms\GridField\GridFieldConfig;
-use SilverStripe\Forms\GridField\GridFieldPageCount;
-use SilverStripe\Forms\GridField\GridFieldPaginator;
-use SilverStripe\Forms\GridField\GridFieldDataColumns;
-use SilverStripe\Forms\GridField\GridFieldFilterHeader;
-use SilverStripe\Forms\GridField\GridFieldToolbarHeader;
-use SilverStripe\Forms\GridField\GridFieldSortableHeader;
-use SilverStripe\Forms\GridField\GridFieldConfig_RecordEditor;
-
-use SilverStripe\Core\Injector\Injector;
-
 use SilverStripe\CMS\Model\SiteTree;
-
-use SilverStripe\Lumberjack\Forms\GridFieldSiteTreeState;
+use SilverStripe\Core\Injector\Injector;
+use SilverStripe\Forms\Form;
+use SilverStripe\Forms\GridField\GridField;
+use SilverStripe\Forms\GridField\GridFieldConfig_RecordEditor;
+use SilverStripe\Forms\Tab;
+use SilverStripe\Forms\TabSet;
 use SilverStripe\Lumberjack\Forms\GridFieldConfig_Lumberjack;
-use SilverStripe\Lumberjack\Forms\GridFieldSiteTreeEditButton;
-use SilverStripe\Lumberjack\Forms\GridFieldSiteTreeAddNewButton;
-
-use SilverShop\HasOneField\GridFieldHasOneButtonRow;
-
 
 class TabsBuilder
 {
-
     /**
-     *
-     * @param array    $arrayOfTabs  contains: [
-     *     TabName => string,
-     *     Title => string,
-     *     List => DataList
-     * ]
-     * @param Form     $form
-     * @param string   $modelToReplace
-     * @param int      $itemsPerPage - optional
+     * @param array $arrayOfTabs  contains: [
+     *                            TabName => string,
+     *                            Title => string,
+     *                            List => DataList
+     *                            ]
+     * @param int   $itemsPerPage - optional
      */
     public static function add_many_tabs(array $arrayOfTabs, Form $form, string $modelToReplace, $itemsPerPage = 100)
     {
@@ -48,13 +27,13 @@ class TabsBuilder
         $fields->insertAfter(
             $modelToReplaceName,
             $parentTab = new TabSet(
-                $modelToReplaceName.'inner',
+                $modelToReplaceName . 'inner',
                 []
             )
         );
         $fields->removeByName($modelToReplaceName);
-        $singleton = Injector::inst()-get($modelToReplace);
-        if($singleton instanceof SiteTree) {
+        $singleton = Injector::inst() - get($modelToReplace);
+        if ($singleton instanceof SiteTree) {
             $config = GridFieldConfig_Lumberjack::create($itemsPerPage);
         } else {
             $config = GridFieldConfig_RecordEditor::create($itemsPerPage);
@@ -76,8 +55,4 @@ class TabsBuilder
             );
         }
     }
-
-
-
-
 }
